94cd77bdb9cc52e4c76f6cc4d43d3027f0aa5f1e
skj092 <sonujha092@gmail.com>
2025-01-31 12:54:53 +0530

    element wise matrix multiplication

--- cppcodes/day4/elememt_wise_mm.cu
+++ cppcodes/day4/elememt_wise_mm.cu
@@ -9,14 +9,24 @@
 - Extra: Add support for different matrix sizes
 */
 __global__
-void mykernel(int *array1, int *array2, int *out, int n){
+void mykernel(int *array1, int *array2, int *out, int n, int m){
     int row = blockIdx.y * blockDim.y + threadIdx.y;
     int col = blockIdx.x * blockDim.x + threadIdx.x;
-    if (row < n && col < n){
-        int idx = row * n + col;
+    if (row < n && col < m){
+        int idx = row * m + col;
         out[idx] = array1[idx] * array2[idx];
     }
 }
+
+#define CHECK_CUDA(call) \
+    do { \
+        cudaError_t err = call; \
+        if (err != cudaSuccess) { \
+            printf("CUDA error at %s:%d - %s\n", __FILE__, __LINE__, cudaGetErrorString(err)); \
+            exit(EXIT_FAILURE); \
+        } \
+    } while (0)
+
 
 inline unsigned int cdiv(unsigned int a, unsigned int b){
     return (a + b - 1) / b;
@@ -24,42 +34,46 @@ inline unsigned int cdiv(unsigned int a, unsigned int b){
 
 
 int main(){
-    int n = 1024;
+    int n = 64;
+    int m = 32;
     int *h_matrix1, *h_matrix2, *h_out_matrix; // host array
     int *d_matrix1, *d_matrix2, *d_out_matrix; // device array
+    // (n x m)
 
     // Seed random number generator
     srand(time(NULL));
 
     // Allocate host memory
-    h_matrix1 = (int *)malloc(n * n * sizeof(int));
-    h_matrix2 = (int *)malloc(n * n * sizeof(int));
-    h_out_matrix = (int *)malloc(n * n * sizeof(int));
+    h_matrix1 = (int *)malloc(n * m * sizeof(int));
+    h_matrix2 = (int *)malloc(n * m * sizeof(int));
+    h_out_matrix = (int *)malloc(n * m * sizeof(int));
 
     // fill the matrix with random numbers (initialize)
     for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
-            h_matrix1[i * n + j] = rand() % 10; // generate number between 0 and 9;
-            h_matrix2[i * n + j] = rand() % 10;
+        for (int j = 0; j < m; j++){
+            h_matrix1[i * m + j] = rand() % 10; // generate number between 0 and 9;
+            h_matrix2[i * m + j] = rand() % 10;
         }
     }
 
     // Allocate device memory
-    cudaMalloc(&d_matrix1, n * n * sizeof(int));
-    cudaMalloc(&d_matrix2, n * n * sizeof(int));
-    cudaMalloc(&d_out_matrix, n * n * sizeof(int));
+    cudaMalloc(&d_matrix1, n * m * sizeof(int));
+    cudaMalloc(&d_matrix2, n * m * sizeof(int));
+    cudaMalloc(&d_out_matrix, n * m * sizeof(int));
 
     // copy data to device
-    cudaMemcpy(d_matrix1, h_matrix1, n * n * sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_matrix2, h_matrix2, n * n * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_matrix1, h_matrix1, n * m * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_matrix2, h_matrix2, n * m * sizeof(int), cudaMemcpyHostToDevice);
 
     // launch kernel
     dim3 threadsperBlock(32, 32);
-    dim3 numBlocks(cdiv(n, threadsperBlock.x), cdiv(n, threadsperBlock.y));
-    mykernel<<<numBlocks, threadsperBlock>>>(d_matrix1, d_matrix2, d_out_matrix, n);
+    dim3 numBlocks(cdiv(n, threadsperBlock.x), cdiv(m, threadsperBlock.y)); // (32, 16)
+    mykernel<<<numBlocks, threadsperBlock>>>(d_matrix1, d_matrix2, d_out_matrix, n, m);
+    CHECK_CUDA(cudaGetLastError());
+    cudaDeviceSynchronize();
 
     // copy data to host
-    cudaMemcpy(h_out_matrix, d_out_matrix, n * n * sizeof(int), cudaMemcpyDeviceToHost);
+    cudaMemcpy(h_out_matrix, d_out_matrix, n * m * sizeof(int), cudaMemcpyDeviceToHost);
 
     // verify
    for (int i = 0; i < n; i++){
@@ -75,5 +89,7 @@ int main(){
     free(h_matrix1);
     free(h_matrix2);
     free(h_out_matrix);
+    return 0;
 
 }
+
